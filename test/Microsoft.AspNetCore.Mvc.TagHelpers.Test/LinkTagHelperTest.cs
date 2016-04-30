d924cd2c6105dbfb9b609e2672b11a18313f0e00
N. Taylor Mullen <nimullen@microsoft.com>
2016-04-29 17:21:33 -0700

    WIP

--- test/Microsoft.AspNetCore.Mvc.TagHelpers.Test/LinkTagHelperTest.cs
+++ test/Microsoft.AspNetCore.Mvc.TagHelpers.Test/LinkTagHelperTest.cs
@@ -842,10 +842,10 @@ namespace Microsoft.AspNetCore.Mvc.TagHelpers
         public void RenderLinkTags_FallbackHref_WithFileVersion_EncodesAsExpected()
         {
             // Arrange
-            var expectedContent = "<link encoded=\"contains &quot;quotes&quot;\" " +
+            var expectedContent = "<link encoded=\"contains \"quotes\"\" " +
                 "href=\"HtmlEncode[[/css/site.css?v=f4OxZX_x_FO5LcGBSKHWXfwtSx-j1ncoSt3SABJtkGk]]\" " +
                 "literal=\"HtmlEncode[[all HTML encoded]]\" " +
-                "mixed=\"HtmlEncode[[HTML encoded]] and contains &quot;quotes&quot;\" />" +
+                "mixed=\"HtmlEncode[[HTML encoded]] and contains \"quotes\"\" />" +
                 Environment.NewLine +
                 "<meta name=\"x-stylesheet-fallback-test\" content=\"\" class=\"HtmlEncode[[hidden]]\" />" +
                 "<script>!function(a,b,c){var d,e=document,f=e.getElementsByTagName(\"SCRIPT\")," +
@@ -960,8 +960,10 @@ namespace Microsoft.AspNetCore.Mvc.TagHelpers
             // Assert
             Assert.Equal("link", output.TagName);
             Assert.Equal("/css/site.css?v=f4OxZX_x_FO5LcGBSKHWXfwtSx-j1ncoSt3SABJtkGk", output.Attributes["href"].Value);
-            Assert.Equal("<link rel=\"stylesheet\" href=\"HtmlEncode[[/base.css?v=f4OxZX_x_FO5LcGBSKHWXfwtSx-j1ncoSt3SABJtkGk]]\" />",
-                output.PostElement.GetContent());
+            var content = HtmlContentUtilities.HtmlContentToString(output.PostElement, new HtmlTestEncoder());
+            Assert.Equal(
+                "<link rel=\"stylesheet\" href=\"HtmlEncode[[/base.css?v=f4OxZX_x_FO5LcGBSKHWXfwtSx-j1ncoSt3SABJtkGk]]\" />",
+                content);
         }
 
         private static ViewContext MakeViewContext(string requestPathBase = null)
